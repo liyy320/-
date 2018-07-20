bd22f2bd93ffd3a7bb4698f6895c4c7c64be1518
ZTY08 <ZTY08@windows10.microdone.cn>
2018-07-20 16:24:06 +0800

    提交

--- MyDemo/src/com/lyy/demo/LyLinkedList/LyLinkedList.java
+++ MyDemo/src/com/lyy/demo/LyLinkedList/LyLinkedList.java
@@ -1,8 +1,6 @@
 package com.lyy.demo.LyLinkedList;
 
-/**
- * ArrayListµ×²ã´úÂë
- * */
+
 public class LyLinkedList
 {
 	
--- MyDemo/src/com/lyy/demo/LyLinkedList/Node.java
+++ MyDemo/src/com/lyy/demo/LyLinkedList/Node.java
@@ -1,6 +1,20 @@
 package com.lyy.demo.LyLinkedList;
 
+@SuppressWarnings("unused")
+/**
+ * LinkedList的节点信息类
+ * */
 public class Node
 {
-	private Node preNode;
+	private Node   prevNode;	//前置节点
+	private Object item;		//元素值
+	private Node   nextNode;	//后置节点
+	
+	//LinkedList节点信息类的有参构造
+	public Node(Node prevNode, Object item, Node nextNode) 
+	{
+		this.prevNode = prevNode;
+		this.item     = item;
+		this.nextNode = nextNode;
+	}
 }
