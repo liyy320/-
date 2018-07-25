1f60d7c8e9e43f1f87a31f2125d6894ee1110725
ZTY08 <ZTY08@windows10.microdone.cn>
2018-07-25 17:23:20 +0800

    1

--- MyDemo/src/com/lyy/util/test.java
+++ MyDemo/src/com/lyy/util/test.java
@@ -1,13 +1,10 @@
 package com.lyy.util;
 
-import java.util.LinkedList;
-import java.util.List;
 
 public class test 
 {
 	public static void main(String[] args)
 	{
-		List list = new LinkedList();
 		
 	}
 }
