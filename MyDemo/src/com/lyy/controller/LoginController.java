64b6d66c937bd95cfbff3512fb729f9036db29a0
ZTY08 <ZTY08@windows10.microdone.cn>
2018-11-22 09:24:51 +0800

    1

--- MyDemo/src/com/lyy/controller/LoginController.java
+++ MyDemo/src/com/lyy/controller/LoginController.java
@@ -18,7 +18,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import com.lyy.utils.comm.BaseController;
 
@@ -97,14 +96,14 @@ public class LoginController extends BaseController
         return renderString(response, returnData);
 	}
 	
-	/** 
-     * 	用户登出 
-     */  
-    @RequestMapping("/logout")  
-    public String logout(HttpServletRequest request){
-
-         SecurityUtils.getSubject().logout();
-
-         return InternalResourceViewResolver.REDIRECT_URL_PREFIX + "/";
-    } 
+//	/** 
+//     * 	用户登出 
+//     */  
+//    @RequestMapping("/logout")  
+//    public String logout(HttpServletRequest request){
+//
+//         SecurityUtils.getSubject().logout();
+//
+//         return InternalResourceViewResolver.REDIRECT_URL_PREFIX + "/";
+//    } 
 }
